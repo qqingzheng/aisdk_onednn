44cd9ea4ebd09ef14c05ac7fe54f7533e2dbc03f
liunian <2476068859@qq.com>
2022-11-17 14:10:34 +0000

    add conv, tanh

--- include/nn/nn.hpp
+++ include/nn/nn.hpp
@@ -101,6 +101,52 @@ namespace aisdk{ namespace nn{
                 }
 
         };
+        
+        template<typename T>
+        class Tanh : Layer<T>{
+            private:
+                Env* env;
+                eltwise_forward primitive;
+                eltwise_forward::primitive_desc pd;
+                Shape<T> md;
+            public:
+                Tanh(Env* env_ptr, const dims& dim, float alpha, float beta){
+                    this->env = env_ptr;
+                    md = Shape<T>(env, dim);
+                    pd = eltwise_forward::primitive_desc({
+                            prop_kind::forward_inference, algorithm::eltwise_tanh,
+                            md.memory_desc, 
+                            alpha, 
+                            beta 
+                        }, env->GetEngine()
+                    );
+                    primitive = eltwise_forward(pd);
+                }
+                Tanh(Env* env_ptr, const dims& dim){
+                    this->env = env_ptr;
+                    md = Shape<T>(env, dim);
+                    pd = eltwise_forward::primitive_desc({
+                            prop_kind::forward_inference, algorithm::eltwise_tanh,
+                            md.memory_desc, 
+                            0.f, 
+                            0.f 
+                        }, env->GetEngine()
+                    );
+                    primitive = eltwise_forward(pd);
+                }
+                void forward(Tensor<T>& input, Tensor<T>& output, bool save_process = false){
+                    primitive.execute(env->GetEngine(),
+                    {
+                        {DNNL_ARG_SRC, input.mem},
+                        {DNNL_ARG_DST, output.mem}
+                    });
+                    if(save_process){
+                        output.save();
+                        env->GetStream().wait();
+                    }
+                }
+
+        };
     }
 
     namespace layer{
@@ -283,6 +329,113 @@ namespace aisdk{ namespace nn{
                     inner_product_args.insert({DNNL_ARG_BIAS, bias_data.mem});
                     inner_product_args.insert({DNNL_ARG_DST, output.mem});
                     primitive.execute(env->GetStream(), inner_product_args);
+                    if(save_process){
+                        output.save();
+                        env->GetStream().wait();
+                    }
+                }
+
+        };
+
+        template<typename T>
+        class Conv : Layer<T>{
+            private:
+                Env* env;
+                Shape<T> md;
+                bool is_weight_reorder = false;
+                convolution_forward primitive;
+                Tensor<T> reordered_weight_data;
+                Tensor<T> weights_data;
+                Tensor<T> bias_data;
+                dims stride;
+                dims padding_l;
+                dims padding_r;
+                dims input_dims;
+                dims output_dims;
+                dims kernel_dims;
+            public:
+                Conv(Env* env_ptr,  dims input_dims, dims output_dims, dims kernel_dims, dims stride, dims padding_l, dims padding_r, const char* weight_file, const char* bias_file){
+                    this->env = env_ptr;
+                    this->input_dims = input_dims;
+                    this->output_dims = output_dims;
+                    this->kernel_dims = kernel_dims;
+                    this->stride = stride;
+                    this->padding_l = padding_l;
+                    this->padding_r = padding_r;
+                    weight_data.init(env, kernel_dims);
+                    bias_data.init(env, {kernel_dims[0]});
+                    reordered_weight_data.init_nomem(env, kernel_dims, true);
+                    std::vector<T> weights(weights_data.shape.size);
+                    std::vector<T> bias(bias_data.shape.size);
+                    std::ifstream weight_in;
+                    std::ifstream bias_in;
+                    weight_in.open(weights_file, std::ios::in | std::ios::binary);
+                    weight_in.read((char*)weights.data(),  sizeof(T)*weights_data.shape.size);
+                    weight_in.close();
+                    bias_in.open(bias_file, std::ios::in | std::ios::binary);
+                    bias_in.read((char*)bias.data(),  sizeof(T)*bias_data.shape.size);
+                    bias_in.close();
+                    init(weights, bias);
+                }
+                Conv(Env* env_ptr, dims input_dims, dims output_dims, dims kernel_dims){
+                    this->env = env_ptr;
+                    this->input_dims = input_dims;
+                    this->output_dims = output_dims;  
+                    this->kernel_dims = kernel_dims;
+                    weights_data.init(env, kernel_dims);
+                    bias_data.init(env, {kernel_dims[0]});
+                    reordered_weight_data.init_nomem(env, kernel_dims, true);
+                }
+                Conv(Env* env_ptr, dims input_dims, dims output_dims, dims kernel_dims, dims stride, dims padding_l, dims padding_r, const std::vector<T>& weights, const std::vector<T>& bias){
+                    this->env = env_ptr;
+                    this->input_dims = input_dims;
+                    this->output_dims = output_dims;
+                    this->kernel_dims = kernel_dims;
+                    this->stride = stride;
+                    this->padding_l = padding_l;
+                    this->padding_r = padding_r;
+                    weights_data.init(env, kernel_dims);
+                    bias_data.init(env, {kernel_dims[0]});
+                    reordered_weight_data.init_nomem(env, kernel_dims, true);
+                    init(weights, bias);
+                }
+                void init(const std::vector<T>& weights, const std::vector<T>& bias){
+                    SetWeight(weights);
+                    SetBias(bias);
+                    Shape<T> shape;
+                    memory::desc src_md = memory::desc(input_dims, shape.GetDtype(), memory::format_tag::any);
+                    memory::desc dst_md  = memory::desc(output_dims, shape.GetDtype(), memory::format_tag::any);
+                    convolution_forward::primitive_desc conv_pd = convolution_forward::primitive_desc({
+                            prop_kind::forward_training, src_md, reordered_weight_data.shape.memory_desc, bias_data.shape.memory_desc, dst_md, stride, padding_l, padding_r}, env->GetEngine());
+
+                    bool need_reorder_weights = linear_pd.weights_desc() != weights_data.shape.memory_desc;
+                    if (need_reorder_weights) {
+                        is_weight_reorder = true;
+                        reordered_weight_data.mem = memory(linear_pd.weights_desc(), env->GetEngine());
+                        reorder(weights_data.mem, reordered_weight_data.mem).execute(env->GetEngine(), weights_data.mem, reordered_weight_data.mem);
+                    }
+                    primitive = inner_product_forward(linear_pd);
+                }
+                void SetWeight(const std::vector<T>& weights){
+                    this->weights_data = weights;
+                }
+                void SetBias(const std::vector<T>& bias){
+                    this->bias_data = bias;
+                }
+                void forward(Tensor<T>& input, Tensor<T>& output, bool save_process = false){
+                    
+                    std::unordered_map<int, memory> conv_args;
+                    conv_args.insert({DNNL_ARG_SRC, input.mem});
+                    if(is_weight_reorder){
+                        conv_args.insert({DNNL_ARG_WEIGHTS, reordered_weight_data.mem});
+                    }else{
+                        conv_args.insert({DNNL_ARG_WEIGHTS, weights_data.mem});
+                    }
+                    conv_args.insert({DNNL_ARG_BIAS, bias_data.mem});
+                    conv_args.insert({DNNL_ARG_DST, output.mem});
+
+                    auto conv_prim = convolution_forward(conv_pd);
+                    conv_prim.execute(env->GetStream(), conv_args);
                     if(save_process){
                         output.save();
                         env->GetStream().wait();
